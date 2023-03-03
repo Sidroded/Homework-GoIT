6e0e9faac213e16760be5debf0d19c8f88b0a297
Sidroded <deinekindaniil21@gmail.com>
2023-03-03 16:39:03 +0100

    Update UserUtils.java

--- src/main/java/org/homework/module10/task2/utils/UserUtils.java
+++ src/main/java/org/homework/module10/task2/utils/UserUtils.java
@@ -12,6 +12,13 @@ import java.util.ArrayList;
 import java.util.IllegalFormatException;
 
 public class UserUtils {
+
+    /**
+     * Read file and return all Users in this file.
+     * @param reader
+     * @return ArrayList<User> users
+     * @throws IOException
+     */
     public static ArrayList<User> getUsersFromFile(BufferedReader reader) throws IOException {
         ArrayList<User> users = new ArrayList<>();
 
@@ -34,6 +41,10 @@ public class UserUtils {
         return users;
     }
 
+    /**
+     * @param users
+     * @return JSONArray with all Users
+     */
     public static JSONArray usersToJsonArray(ArrayList<User> users) {
         JSONArray jsonArray = new JSONArray();
 
@@ -47,6 +58,13 @@ public class UserUtils {
         return jsonArray;
     }
 
+    /**
+     * If constant is JSON convert users to Json by Gson
+     * If constant is TO_STRING convert users to Json by override method toString()
+     * @param users
+     * @param constant
+     * @return ArrayList<String> of users in Json format
+     */
     public static ArrayList<String> usersToJsonArrayList(ArrayList<User> users, Type constant) {
         ArrayList<String> jsonUsersStringList = new ArrayList<>();
 
