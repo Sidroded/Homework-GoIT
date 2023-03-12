c14053d236dd6aa778c228d36619efe213e11e14
Sidroded <deinekindaniil21@gmail.com>
2023-03-12 16:19:05 +0100

    Fix task 3 module 11

--- src/main/java/org/homework/module11/task3/MainTaskThree.java
+++ src/main/java/org/homework/module11/task3/MainTaskThree.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 public class MainTaskThree {
     public static void main(String[] args) {
-        List<String> array = Arrays.asList("1, 2, 0", "4, 5");
+        List<String> array = Arrays.asList("1, 2, 0", "4, 5", "10, 11", "33");
 
         String result = array.stream()
                 .flatMap(string -> Arrays.stream(string.split(",\\s*")))
+                .map(Integer :: parseInt)
                 .sorted()
+                .map(String :: valueOf)
                 .collect(Collectors.joining(", "));
 
         System.out.println(result);
