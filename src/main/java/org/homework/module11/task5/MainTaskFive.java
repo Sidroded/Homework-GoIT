92bd6caa99deb98f1fa8f0a9adfeb4f73b3f8676
Sidroded <deinekindaniil21@gmail.com>
2023-03-12 14:45:16 +0100

    Update MainTaskFive.java

--- src/main/java/org/homework/module11/task5/MainTaskFive.java
+++ src/main/java/org/homework/module11/task5/MainTaskFive.java
@@ -1,11 +1,11 @@
 package org.homework.module11.task5;
 
-import java.util.Objects;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 public class MainTaskFive {
     public static void main(String[] args) {
-        Integer[] evenNumbers = {0, 2, 4, 6, 8, 10};
+        Integer[] evenNumbers = {0, 2, 4, 6, 8, 10, 12, 14};
         Integer[] oddNumbers = {1, 3, 5, 7, 9};
 
         Stream<Integer> evenStream = Stream.of(evenNumbers);
@@ -16,25 +16,15 @@ public class MainTaskFive {
     }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        var iterator1 = first.iterator();
-        var iterator2 = second.iterator();
+        Iterator<T> firstIterator = first.iterator();
+        Iterator<T> secondIterator = second.iterator();
 
-        return Stream.iterate(
-                        new Object[]{iterator1.next(), iterator2.next()}, Objects::nonNull,
-                        objects -> {
-                            try {
-                                objects[0] = iterator1.next();
-                                objects[1] = iterator2.next();
-                                return objects;
-                            } catch (Exception e) {
-                                return null;
-                            }
-                        })
-                .flatMap(objects -> {
-                    if (objects != null) {
-                        return Stream.of((T) objects[0], (T) objects[1]);
-                    }
-                    return Stream.empty();
-                });
+        Stream<T> zipped = Stream.empty();
+
+        while (firstIterator.hasNext() && secondIterator.hasNext()) {
+            zipped = Stream.concat(zipped, Stream.of(firstIterator.next(), secondIterator.next()));
+        }
+
+        return zipped;
     }
 }
