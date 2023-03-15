55dd0f4cbccb6fdecf4fa7ed837a6866abacb5b6
Sidroded <deinekindaniil21@gmail.com>
2023-03-15 20:17:17 +0100

    Dont finish task 2 module 12

--- src/main/java/org/homework/module12/task2/MainTaskTwo.java
+++ src/main/java/org/homework/module12/task2/MainTaskTwo.java
@@ -1,13 +1,12 @@
 package org.homework.module12.task2;
 
-import org.homework.module12.task2.threads.*;
-
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainTaskTwo {
-    public static int count = 1;
+    public static volatile int count = 1;
     public static int num;
+    public static final Object MONITOR = new Object();
     public static BlockingQueue<String> fizzQueue = new LinkedBlockingQueue<>();
     public static BlockingQueue<String> buzzQueue = new LinkedBlockingQueue<>();
     public static BlockingQueue<String> fizzBuzzQueue  = new LinkedBlockingQueue<>();
@@ -16,10 +15,67 @@ public class MainTaskTwo {
     public static void main(String[] args) throws InterruptedException {
         num = TaskUtils.getNum();
 
-        Thread a = new Thread(new A());
-        Thread b = new Thread(new B());
-        Thread c = new Thread(new C());
-        Thread d = new Thread(new D(num));
+        Thread a = new Thread(() -> {
+            while (true) {
+                synchronized (MONITOR) {
+                    if (isCountGreaterThanNum()) {
+                        return;
+                    } else if (count % 3 == 0 && count % 5 != 0) {
+                        fizzQueue.add("fizz");
+                        count++;
+                    }
+                }
+            }
+        });
+
+        Thread b = new Thread(() -> {
+            while (true) {
+                synchronized (MONITOR) {
+                    if (isCountGreaterThanNum()) {
+                        return;
+                    } else if (count % 3 != 0 && count % 5 == 0) {
+                        buzzQueue.add("buzz");
+                        count++;
+                    }
+                }
+            }
+        });
+        Thread c = new Thread(() -> {
+            while (true){
+                synchronized (MONITOR) {
+                    if (isCountGreaterThanNum()) {
+                        return;
+                    } else if (count % 3 == 0 && count % 5 == 0) {
+                        fizzBuzzQueue.add("fizzbuzz");
+                        count++;
+                    }
+                }
+            }
+        });
+        Thread d = new Thread(() -> {
+            StringBuilder stringBuffer = new StringBuilder();
+
+            synchronized (MONITOR) {
+                while (true) {
+                    if (isCountGreaterThanNum()) {
+                        System.out.println(stringBuffer);
+                        return;
+                    } else if (!fizzQueue.isEmpty()) {
+                        stringBuffer.append("fizz, ");
+                        MainTaskTwo.fizzQueue.remove();
+                    } else if (!MainTaskTwo.buzzQueue.isEmpty()) {
+                        stringBuffer.append("buzz, ");
+                        MainTaskTwo.buzzQueue.remove();
+                    } else if (!MainTaskTwo.fizzBuzzQueue.isEmpty()) {
+                        stringBuffer.append("fizzbuzz, ");
+                        MainTaskTwo.fizzBuzzQueue.remove();
+                    } else {
+                        stringBuffer.append(MainTaskTwo.count).append(", ");
+                        MainTaskTwo.count++;
+                    }
+                }
+            }
+        });
 
         a.start();
         b.start();
@@ -32,17 +88,6 @@ public class MainTaskTwo {
         d.join();
     }
 
-    public static synchronized void addToFizzQueue(String item) {
-        fizzQueue.add(item);
-    }
-
-    public static synchronized void addToBuzzQueue(String item) {
-        buzzQueue.add(item);
-    }
-
-    public static synchronized void addToFizzBuzzQueue(String item) {
-        fizzBuzzQueue.add(item);
-    }
 
     public static synchronized boolean isCountGreaterThanNum() {
         return count > num;
