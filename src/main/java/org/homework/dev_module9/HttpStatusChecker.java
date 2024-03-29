928da7d8bef2c30c3125397c4c758e573c78f240
Sidroded <deinekindaniil21@gmail.com>
2024-03-29 14:50:50 +0100

    feat: Finish dev module 10 (Web module)

--- src/main/java/org/homework/dev_module9/HttpStatusChecker.java
+++ src/main/java/org/homework/dev_module9/HttpStatusChecker.java
@@ -2,7 +2,6 @@ package org.homework.dev_module9;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpStatusChecker {
